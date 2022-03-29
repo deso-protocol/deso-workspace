caa6617c04dd9048776b9f2f78ab40122e4a7302
DeSoDog <ty@deso.org>
2022-03-29 15:47:18 -0500

    Mobile!

--- apps/developer-hub/src/components/layout/Header/HubButton.tsx
+++ apps/developer-hub/src/components/layout/Header/HubButton.tsx
@@ -2,13 +2,15 @@ import { Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { CHAPTERS } from '../../../chapters/ChapterHelper/Chapter.models';
 import Logo from '../../../assets/deso-logo.png';
+import { BrowserView, MobileView } from 'react-device-detect';
 export const HubButton = () => {
   return (
     <Link to={CHAPTERS.ABOUT.route} className="hover:text-[#ffc08c]">
       <Typography variant="h6" component="div">
         <img className="max-h-[15px] mr-2 inline" src={Logo} />
         <div className="inline mt-4 max-h-[32px] text-base font-semibold">
-          Developer Hub (beta)
+          <BrowserView>Developer Hub (beta)</BrowserView>
+          <MobileView></MobileView>
         </div>
       </Typography>
     </Link>
