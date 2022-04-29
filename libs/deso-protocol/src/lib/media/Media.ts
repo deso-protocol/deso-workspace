e7c6e7ae6556e39adfad66320c45cfc899cfa7ff
DeSoDog <ty@deso.org>
2022-04-29 15:38:10 -0500

    Added image upload

--- libs/deso-protocol/src/lib/media/Media.ts
+++ libs/deso-protocol/src/lib/media/Media.ts
@@ -9,7 +9,10 @@ import {
 } from 'deso-protocol-types';
 import { Node } from '../Node/Node';
 import { Identity } from '../identity/Identity';
-
+import { resolve } from 'path';
+import { throwErrors } from '../../utils/utils';
+import * as tus from 'tus-js-client';
+import { uploadVideo } from './Tus.config';
 export class Media {
   private node: Node;
   private identity: Identity;
@@ -20,22 +23,41 @@ export class Media {
 
   public async uploadImage(
     request: Partial<UploadImageRequest>
-  ): Promise<UploadImageResponse> {
-    const endpoint = 'upload-image';
-    const formData = new FormData();
-    formData.append('file', request.file as Blob);
-    formData.append(
-      'UserPublicKeyBase58Check',
-      request.UserPublicKeyBase58Check as string
-    );
-    formData.append('JWT', request.JWT as string);
-    return await axios.post(`${this.node.getUri()}/${endpoint}`, request);
+  ): Promise<UploadImageResponse | void> {
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const nodeRef = this.node;
+    const JWT = await this.identity.getJwt();
+    if (document && !request.file) {
+      const input = document.createElement('input');
+      input.type = 'file';
+      input.click();
+      const waitForSelection = new Promise((resolve, reject) => {
+        input.onchange = () => {
+          input && input.files && input.files[0]
+            ? resolve(input.files[0])
+            : reject(new Error('No File selected'));
+        };
+      });
+
+      return await waitForSelection.then(function (file) {
+        if (file) {
+          request.file = file as File;
+          return uploadImageHelper(request, nodeRef, JWT);
+        }
+        return;
+      });
+    } else if (request.file) {
+      return uploadImageHelper(request, nodeRef, JWT);
+    }
+    throwErrors(['UserPublicKeyBase58Check', 'file'], request);
   }
 
-  public async uploadVideo(
+  private async uploadVideo(
     request: Partial<UploadImageRequest>
   ): Promise<UploadImageResponse> {
-    const endpoint = 'get-nfts-for-user';
+    // TODO
+    const endpoint = '';
+    // uploadVideo(`${this.node.getUri()}/${endpoint}`);
     return await axios.post(`${this.node.getUri()}/${endpoint}`, request);
   }
 
@@ -53,3 +75,21 @@ export class Media {
     return await axios.post(`${this.node.getUri()}/${endpoint}`, request);
   }
 }
+
+const uploadImageHelper = async (
+  request: Partial<UploadImageRequest>,
+  node: Node,
+  JWT: string
+): Promise<UploadImageResponse> => {
+  const endpoint = 'upload-image';
+  const formData = new FormData();
+  formData.append('file', request.file as File);
+  formData.append(
+    'UserPublicKeyBase58Check',
+    request.UserPublicKeyBase58Check as string
+  );
+  formData.append('JWT', JWT);
+  return await (
+    await axios.post(`${node.getUri()}/${endpoint}`, formData)
+  ).data;
+};
---
+++ libs/deso-protocol/src/lib/media/Tus.config.ts
@@ -0,0 +1,37 @@
+import { rejects } from 'assert';
+import * as tus from 'tus-js-client';
+
+export const uploadVideo = (path: string, file: File): void => {
+  //   let mediaId;
+  //   const onResponse = new Promise((resolve, reject) => {
+  //     const options = {
+  //       endpoint: path,
+  //       chunkSize: 50 * 1024 * 1024, // Required a minimum chunk size of 5MB, here we use 50MB.
+  //       uploadSize: file.size,
+  //       onError: function (error) {
+  //         throw new Error(error);
+  //       },
+  //       onProgress: function (bytesUploaded, bytesTotal) {
+  //         //   comp.videoUploadPercentage = ((bytesUploaded / bytesTotal) * 100).toFixed(
+  //         // 2
+  //         //   );
+  //       },
+  //       onSuccess: function () {
+  //         // Construct the url for the video based on the videoId and use the iframe url.
+  //         const postVideoSrc = `https://iframe.videodelivery.net/${mediaId}`;
+  //       },
+  //       onAfterResponse: function (req, res) {
+  //         // The stream-media-id header is the video Id in Cloudflare's system that we'll need to locate the video for streaming.
+  //         const mediaIdHeader = res.getHeader('stream-media-id');
+  //         let mediaId;
+  //         if (mediaIdHeader) {
+  //           mediaId = mediaIdHeader;
+  //           resolve(mediaId as string);
+  //         } else {
+  //           reject(new Error('no media id'));
+  //         }
+  //       },
+  //     };
+  //   });
+  //   return onResponse as Promise<string>;
+};
