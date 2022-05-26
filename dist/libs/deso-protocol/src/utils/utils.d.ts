2d548af1a375419b4d5832bf4803af28224d7b4c
DeSoDog <ty@deso.org>
2022-05-26 16:42:39 -0500

    Build

--- dist/libs/deso-protocol/src/utils/utils.d.ts
+++ dist/libs/deso-protocol/src/utils/utils.d.ts
@@ -3,3 +3,8 @@ export declare const uuid: () => string;
 export declare const convertToHex: (str: string) => string;
 export declare const convertExtraDataToHex: (extraData: Omit<AppendExtraDataRequest, 'TransactionHex'>) => Omit<AppendExtraDataRequest, "TransactionHex">;
 export declare const throwErrors: <G, K extends keyof G>(requiredAttributes: K[], request: G) => void;
+export declare const assignDefaults: <G, K extends keyof G>(attributesWithDefaults: AssignDefaultsInterface<G, K>[], request: G) => G;
+export interface AssignDefaultsInterface<G, K extends keyof G> {
+    name: K;
+    default: G[K];
+}
