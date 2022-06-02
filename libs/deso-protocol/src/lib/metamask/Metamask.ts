33ea053f654b34a816fe9f7a8dd16273e3451160
DeSoDog <ty@deso.org>
2022-06-02 10:33:48 -0500

    Added EC

--- libs/deso-protocol/src/lib/metamask/Metamask.ts
+++ libs/deso-protocol/src/lib/metamask/Metamask.ts
@@ -1,6 +1,8 @@
 import { Node } from '../Node/Node';
 import { Identity } from '../identity/Identity';
+import { ec } from 'elliptic';
 // import { IdentityDeriveParams } from 'deso-protocol-types';
+import bs58check from 'bs58check';
 import { ethers } from 'ethers';
 // import * as bip39 from 'bip39';
 export class Metamask {
@@ -28,16 +30,19 @@ export class Metamask {
     };
     const mainnet = 'mainnet';
     const testnet = 'testnet';
-    // 1. goal of this is to generate a random derived key
+    // 1.1 goal of this is to generate a random derived key
     const entropy = ethers.utils.randomBytes(16);
     const dMnemonic = ethers.utils.entropyToMnemonic(entropy);
     const dKeyChain = ethers.utils.HDNode.fromMnemonic(dMnemonic);
-    console.log({ entropy, dMnemonic, dKeyChain });
-    console.log(dKeyChain.privateKey);
-    const prefix = PUBLIC_KEY_PREFIXES.mainnet.deso
-    ethers.utils.
-    dKeyChain.publicKey
-    const desoPublicKey = 
+    // 1.2 turn it into a deso key
+    const prefix = PUBLIC_KEY_PREFIXES.mainnet.deso;
+    const e = new ec('secp256k1');
+    const ecKey = e.keyFromPrivate(dKeyChain.privateKey);
+    ecKey.getPublic().encode('array', true);
+
+    // ethers.utils.
+    // dKeyChain.publicKey
+    // const desoPublicKey =
 
     //2. expiration block
 
