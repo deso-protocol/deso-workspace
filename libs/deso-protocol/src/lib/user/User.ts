23a914627c7d4176d60b7db01c78bfbb63aba67d
TylerFischer@Deso <98359380+DeSoDog@users.noreply.github.com>
2022-09-13 14:52:14 -0500

    fix(deso-protocol/utils): tsconfig update (#61)
    tsconfig was wrongly compiling the hdkey package

--- libs/deso-protocol/src/lib/user/User.ts
+++ libs/deso-protocol/src/lib/user/User.ts
@@ -37,8 +37,13 @@ export class User {
     ).data;
   }
 
-  public getSingleProfilePicture(PublicKeyBase58Check: string, fallbackImageUrl?: string) {
-    return `${this.node.getUri()}/get-single-profile-picture/${PublicKeyBase58Check}${fallbackImageUrl ? `?fallback=${fallbackImageUrl}`: ''}`;
+  public getSingleProfilePicture(
+    PublicKeyBase58Check: string,
+    fallbackImageUrl?: string
+  ) {
+    return `${this.node.getUri()}/get-single-profile-picture/${PublicKeyBase58Check}${
+      fallbackImageUrl ? `?fallback=${fallbackImageUrl}` : ''
+    }`;
   }
 
   public async getSingleProfile(
