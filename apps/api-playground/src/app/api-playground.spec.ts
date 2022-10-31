4a5b253a87a2df95ff087299e32f5d8f44355906
DeSoDog <ty@deso.org>
2022-10-31 15:56:31 -0500

    Update test

--- apps/api-playground/src/app/api-playground.spec.ts
+++ apps/api-playground/src/app/api-playground.spec.ts
@@ -1,6 +1,6 @@
 // import { PORT, runDefaultNodeApp } from './api-playground';
-// describe('apiPlayground', () => {
-it('should work', () => {
-  expect(true).toBeTruthy();
+describe('apiPlayground', () => {
+  it('should work', () => {
+    expect(true).toBeTruthy();
+  });
 });
-// }); // doesn't make sense to test here, but going to leave this as an example
