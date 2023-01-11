f82e8d2fdc5004628000cce51fb1805917fc6b13
Jackson Dean <jckson.dean@gmail.com>
2023-01-10 18:53:37 -0800

    use the canonical option when signing

--- libs/identity/src/lib/crypto-utils.ts
+++ libs/identity/src/lib/crypto-utils.ts
@@ -64,7 +64,7 @@ export const sign = (
 ): string => {
   const transactionBytes = Buffer.from(txHex, 'hex');
   const transactionHash = Buffer.from(sha256.x2(transactionBytes), 'hex');
-  const signature = keyPair.sign(transactionHash);
+  const signature = keyPair.sign(transactionHash, { canonical: true });
   const signatureBytes = Buffer.from(signature.toDER());
   const signatureLength = uvarint64ToBuf(signatureBytes.length);
 
