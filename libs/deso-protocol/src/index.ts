8812751df0dd8aa8b7982ba3286308e81947e995
Jackson Dean <jckson.dean@gmail.com>
2023-02-25 15:02:45 -0800

    fix side-effects and publish es6 modules

--- libs/deso-protocol/src/index.ts
+++ libs/deso-protocol/src/index.ts
@@ -1,16 +1,3 @@
-import { api } from '@deso-core/data';
-import { identity } from '@deso-core/identity';
-
-/**
- * Sync up the API and Identity modules with the same nodeURI. This can be overridden by
- * passing a different nodeURI to the configure call. If we never pass a nodeURI as a
- * configuration option, the default configured here will be used.
- */
-api.configure({ nodeURI: identity.nodeURI });
-
-/**
- * Public API
- */
 export * from '@deso-core/data';
 export * from '@deso-core/identity';
 export * from 'deso-protocol-types';
